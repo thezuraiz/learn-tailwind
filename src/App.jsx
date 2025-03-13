0bdc41e29ef017abde296fa1b82e7b4b6dd1c8f2
The Zuraiz <thezuraiz@gmail.com>
2025-03-13 14:31:06 +0500

    Creating Components

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import {
+  Footer,
+  Hero,
+  CustomerReviews,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
+function App() {
+  return (
+    <>
+      <main className="relative">
+        Nav
+        <section className="xl:padding-1 wide:padding-r padding-b">
+          <Hero />
+        </section>
+        <section className="padding">
+          <PopularProducts />
+        </section>
+        <section className="padding">
+          <SuperQuality />
+        </section>
+        <section className="padding-x py-10">
+          <Services />
+        </section>
+        <section className="padding">
+          <SpecialOffer />
+        </section>
+        <section className="bg-pale-blue padding">
+          <CustomerReviews />
+        </section>
+        <section className="padding-x py-16 w-full sm:py-32">
+          <Subscribe />
+        </section>
+        <footer className="bg-black padding-x padding-t pb-8">
+          <Footer />
+        </footer>
+      </main>
+    </>
+  );
+}
+
+export default App;
